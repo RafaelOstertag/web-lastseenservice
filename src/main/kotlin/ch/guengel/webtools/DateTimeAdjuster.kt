ae846aa9513e8a36709d4f957982d5c0452da0b9
Rafael Ostertag <rafi@guengel.ch>
2018-07-18 20:00:28 +0200

    Add DateTimeAdjuster

---
+++ src/main/kotlin/ch/guengel/webtools/DateTimeAdjuster.kt
@@ -0,0 +1,67 @@
+package ch.guengel.webtools
+
+import org.joda.time.DateTime
+import org.joda.time.Days.days
+import org.joda.time.Period.*
+import org.joda.time.ReadablePeriod
+
+/**
+ * Given a DateTime, it calculates a new DateTime based on a time specification.
+ *
+ * The time specification has following form:
+ *
+ *     <days>d<hours>h<minutes>m<seconds>s
+ *
+ * For instance
+ *
+ *     1d7s
+ *
+ * would adjust the given DateTime by one day and seven seconds
+ */
+class DateTimeAdjuster(private val current: DateTime) {
+    private val regex =
+        Regex("(?:(?<DAYS>-?[1-9][0-9]?)d|(?<HOURS>-?(?:[1-9]|1[0-9]|2[0-3]))h|(?<MINUTES>-?(?:[1-9]|[1-5][0-9]))m|(?<SECONDS>-?(?:[1-9]|[1-5][0-9]))s|\\s+)+")
+
+    fun by(timeSpecString: String): DateTime {
+        val match = regex.matchEntire(timeSpecString) ?: throw IllegalArgumentException("Illegal time spec pattern")
+
+        val durations = resolveMatchToPeriods(match)
+
+        return durations.fold(current, { acc, readablePeriod -> acc.minus(readablePeriod) })
+    }
+
+    private fun resolveMatchToPeriods(match: MatchResult): List<ReadablePeriod> {
+        return Converters
+            .values()
+            .map {
+                it.converter()(match.groups[it.name]?.value)
+            }
+    }
+
+}
+
+
+internal enum class Converters {
+    DAYS {
+        override fun converter(): (String?) -> ReadablePeriod {
+            return { x -> if (x == null) days(0) else days(x.toInt()) }
+        }
+    },
+    HOURS {
+        override fun converter(): (String?) -> ReadablePeriod {
+            return { x -> if (x == null) hours(0) else hours(x.toInt()) }
+        }
+    },
+    MINUTES {
+        override fun converter(): (String?) -> ReadablePeriod {
+            return { x -> if (x == null) minutes(0) else minutes(x.toInt()) }
+        }
+    },
+    SECONDS {
+        override fun converter(): (String?) -> ReadablePeriod {
+            return { x -> if (x == null) seconds(0) else seconds(x.toInt()) }
+        }
+    };
+
+    abstract fun converter(): (String?) -> ReadablePeriod
+}
---
+++ src/test/kotlin/ch/guengel/webtools/DateTimeAdjusterTest.kt
@@ -0,0 +1,60 @@
+package ch.guengel.webtools
+
+import org.joda.time.DateTime
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DateTimeAdjusterTest {
+    private val baseTime = DateTime(2006, 7, 8, 12, 0, 0)
+    private val subject = DateTimeAdjuster(baseTime)
+
+    @Test
+    fun `full specification`() {
+        val actual = subject.by("6d5h4m3s")
+        val expected = DateTime(2006, 7, 2, 6, 55, 57)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `hour minute second specfication`() {
+        val actual = subject.by("5h4m3s")
+        val expected = DateTime(2006, 7, 8, 6, 55, 57)
+    }
+
+    @Test
+    fun `minute second specfication`() {
+        val actual = subject.by("4m3s")
+        val expected = DateTime(2006, 7, 8, 11, 55, 57)
+    }
+
+    @Test
+    fun `second specfication`() {
+        val actual = subject.by("3s")
+        val expected = DateTime(2006, 7, 8, 11, 59, 57)
+    }
+
+    @Test
+    fun `minute specification`() {
+        val actual = subject.by("4m")
+        val expected = DateTime(2006, 7, 8, 11, 56, 0)
+    }
+
+    @Test
+    fun `hour specification`() {
+        val actual = subject.by("5h")
+        val expected = DateTime(2006, 7, 8, 7, 0, 0)
+    }
+
+    @Test
+    fun `day specification`() {
+        val actual = subject.by("6d")
+        val expected = DateTime(2006, 7, 2, 12, 0, 0)
+    }
+
+    @Test
+    fun `mixed specification`() {
+        val actual = subject.by("6d1s")
+        val expected = DateTime(2006, 7, 2, 11, 59, 59)
+    }
+}
