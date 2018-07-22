754bbfa1c45d509c5f9a3d43d5035bc23b30ad05
Rafael Ostertag <rafi@guengel.ch>
2018-07-22 20:48:22 +0200

    Print git version

--- src/main/kotlin/ch/guengel/webtools/main.kt
+++ src/main/kotlin/ch/guengel/webtools/main.kt
@@ -3,8 +3,26 @@ package ch.guengel.webtools
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.LoggerFactory
+import java.util.*
+
+fun printGitVersion() {
+    val logger = LoggerFactory.getLogger("git-info")
+
+    try {
+        val properties = Properties()
+        val gitPropertiesStream = properties::class.java.getResourceAsStream("/git.properties")
+        gitPropertiesStream.use {
+            properties.load(gitPropertiesStream)
+            properties.forEach { name, value -> logger.info("$name: $value") }
+        }
+    } catch (e: Throwable) {
+        logger.warn("Unable to read git.properties: {}", e.message ?: "null")
+    }
+}
 
 fun main(args: Array<String>) {
+    printGitVersion()
     embeddedServer(Netty, commandLineEnvironment(args)).start(true)
 }
 
