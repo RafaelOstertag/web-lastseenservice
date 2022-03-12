557fe56a81861335bc2a3d8eefb93282c2167099
Rafael Ostertag <rafi@guengel.ch>
2022-03-12 16:00:21 +0100

    Use dedicated coroutine scope

--- src/main/kotlin/ch/guengel/webtools/services/HealthGrpcService.kt
+++ src/main/kotlin/ch/guengel/webtools/services/HealthGrpcService.kt
@@ -4,7 +4,8 @@ import ch.guengel.lastseenservice.HealthGrpc
 import ch.guengel.lastseenservice.HealthOuterClass
 import io.grpc.Status
 import io.grpc.stub.StreamObserver
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newFixedThreadPoolContext
@@ -12,28 +13,30 @@ import org.slf4j.LoggerFactory
 
 class HealthGrpcService : HealthGrpc.HealthImplBase() {
 
-    override fun check(request: HealthOuterClass.HealthCheckRequest?,
-                       responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?) {
+    override fun check(
+        request: HealthOuterClass.HealthCheckRequest?,
+        responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?,
+    ) {
 
         when (request?.service) {
-            "" -> sendSingleResponse(makeResponse(HealthOuterClass.HealthCheckResponse.ServingStatus.SERVING),
-                    responseObserver)
-            serviceName -> sendSingleResponse(makeResponse(HealthOuterClass.HealthCheckResponse.ServingStatus.SERVING),
-                    responseObserver)
+            "", serviceName -> sendSingleResponse(makeResponse(HealthOuterClass.HealthCheckResponse.ServingStatus.SERVING),
+                responseObserver)
             else ->
                 sendErrorStatus(Status.NOT_FOUND, responseObserver)
         }
         logger.info("Sent health response")
     }
 
-    override fun watch(request: HealthOuterClass.HealthCheckRequest?,
-                       responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?) {
+    override fun watch(
+        request: HealthOuterClass.HealthCheckRequest?,
+        responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?,
+    ) {
         if (request?.service != "" && request?.service != serviceName) {
             sendErrorStatus(Status.NOT_FOUND, responseObserver)
             return
         }
 
-        GlobalScope.launch(watchCoroutineContext) {
+        grpcStreamScope.launch(dispatcher) {
             val response = makeResponse(HealthOuterClass.HealthCheckResponse.ServingStatus.SERVING)
             try {
                 while (true) {
@@ -50,18 +53,24 @@ class HealthGrpcService : HealthGrpc.HealthImplBase() {
     companion object {
         private val logger = LoggerFactory.getLogger(HealthGrpcService::class.java)
         private const val serviceName = "ch.guengel.lastseenservice.LastSeen"
-        private val watchCoroutineContext = newFixedThreadPoolContext(5, "watch-context")
+
+        @OptIn(DelicateCoroutinesApi::class)
+        private val dispatcher = newFixedThreadPoolContext(5, "watch-context")
+        private val grpcStreamScope = CoroutineScope(dispatcher)
     }
 }
 
-private fun sendSingleResponse(response: HealthOuterClass.HealthCheckResponse,
-                               responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?) {
+private fun sendSingleResponse(
+    response: HealthOuterClass.HealthCheckResponse,
+    responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?,
+) {
     responseObserver?.onNext(response)
     responseObserver?.onCompleted()
 }
 
 private fun sendErrorStatus(status: Status, responseObserver: StreamObserver<HealthOuterClass.HealthCheckResponse>?) =
-        responseObserver?.onError(status.asRuntimeException())
+    responseObserver?.onError(status.asRuntimeException())
 
-private fun makeResponse(status: HealthOuterClass.HealthCheckResponse.ServingStatus) = HealthOuterClass.HealthCheckResponse.newBuilder().setStatus(
+private fun makeResponse(status: HealthOuterClass.HealthCheckResponse.ServingStatus) =
+    HealthOuterClass.HealthCheckResponse.newBuilder().setStatus(
         status).build()
